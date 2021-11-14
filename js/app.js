8904f07d7749599b2219b2113e4a5184385ea5ed
Marcelo España <marcelonicolasespana@gmail.com>
2021-11-14 16:40:54 -0300

    Tercer commit Diseño Responsive.

---
+++ js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    eventListeners();
+})
+
+function eventListeners() {
+    const mobilMenu = document.querySelector('mobile-menu')
+    mobilMenu.addEventListener('click', navegacionResponsive)
+}
+
+function navegacionResponsive () {
+    const navegacion = document.querySelector('.navegacion');
+
+    if(navegacion.classList.contains('mostrar')) {
+        navegacion.classList.remove('mostrar');
+    } else {
+        navegacion.classlist.remove('mostrar');
+    }
+}
